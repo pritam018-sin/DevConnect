d7b447c52b9215897819575ef64bfeefb8d01b8b
pritam <wepritam.18@gmail.com>
2025-10-30 20:14:12 +0530

    Refactor app.js for improved code consistency and organization

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,39 @@
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import http from 'http';
+import { setupSocket } from './socket/socket.js';
 
-import cookieParser from "cookie-parser";
-
-const app = express();
+const app = express(); // <-- pehle express app banao
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
 }));
-app.use(express.json({limit: '30kb'}));
-app.use(express.urlencoded({extended: true, limit: '30kb'}));
-app.use(express.static("public"));// Serve static files from the 'public' directory
+
+app.use(express.json({ limit: '30kb' }));
+app.use(express.urlencoded({ extended: true, limit: '30kb' }));
+app.use(express.static('public'));
 app.use(cookieParser());
 
-import userRoutes from "./routes/user.Routes.js"
-import postRoutes from "./routes/post.Routes.js"
-import commentRoutes from "./routes/comment.Routes.js"
-import projectRoutes from "./routes/project.Routes.js"
-import likeRoutes from "./routes/like.Routes.js"
-import followRoutes from "./routes/follow.Routes.js"
+// Routes import
+import userRoutes from './routes/user.Routes.js';
+import postRoutes from './routes/post.Routes.js';
+import commentRoutes from './routes/comment.Routes.js';
+import projectRoutes from './routes/project.Routes.js';
+import likeRoutes from './routes/like.Routes.js';
+import followRoutes from './routes/follow.Routes.js';
 
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/comments", commentRoutes);
-app.use("/api/v1/projects", projectRoutes);
-app.use("/api/v1/likes", likeRoutes);
-app.use("/api/v1/follows", followRoutes);
+// Routes use
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/comments', commentRoutes);
+app.use('/api/v1/projects', projectRoutes);
+app.use('/api/v1/likes', likeRoutes);
+app.use('/api/v1/follows', followRoutes);
 
-export default app;
+// HTTP + Socket.io setup
+const server = http.createServer(app);
+setupSocket(server);
+
+export default server;
